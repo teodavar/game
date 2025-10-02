c5fdee068821148aaf05849136a7a300b727020c
SeanessSA-social <14kannemeyer@gmail.com>
2025-10-01 21:44:21 -0400

    Small Changes
    - Added some fonts
    - Added skip button to intro

--- scripts/intro_scene.gd
+++ scripts/intro_scene.gd
@@ -2,3 +2,12 @@ extends Control
 
 func _on_video_stream_player_finished():
 	get_tree().change_scene_to_file("res://scene/main.tscn")
+	
+
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://scene/main.tscn")
+
+
+func _on_button_mouse_entered():
+	$Hover.play()
