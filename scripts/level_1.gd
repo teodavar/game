62b958815f700c804a6cbbc62d727b57bae03716
TT <teodav@otenet.gr>
2025-10-03 00:12:53 -0400

    explosions

--- scripts/level_1.gd
+++ scripts/level_1.gd
@@ -18,7 +18,7 @@ func play(start_time=0):
 	huge_asteroid.rotation+=PI
 	generate_field(comet_scene,flip(Vector2(200,0)),flip(PI/2),300,0.2,5,start_time+1,3,Vector2(200,0))
 	generate_field(comet_scene,flip(Vector2(650,0)),flip(PI/2),300,0.3,4,start_time+6.5,3,Vector2(150,0))
-	generate_field(huge_asteroid,flip(Vector2(1100,1500)),flip(-PI/2-PI/24),75,0,0,start_time+0,1,Vector2(50,0))
+	generate_field(huge_asteroid,flip(Vector2(1100,1500)),flip(-PI/2-PI/24),85,0,0,start_time+0,1,Vector2(50,0),Vector2(0.9,1.4))
 	generate_field(comet_scene,flip(Vector2(-100,690)),flip(-PI/5),200,0.2,3,start_time+8,3,Vector2(0,50))
 	generate_field(comet_scene,flip(Vector2(-100,690)),flip(-PI/5),400,0.2,3,start_time+9,3,Vector2(0,50))
 	generate_field(comet_scene,flip(Vector2(-100,200)),flip(-PI/16),200,0.2,3,start_time+10,3,Vector2(0,50))
--- scripts/level_2.gd
+++ scripts/level_2.gd
@@ -16,7 +16,7 @@ func play(start_time=0):
 	var huge_asteroid=self.asteroid_scene.instantiate()
 	huge_asteroid.reshape(4)
 	huge_asteroid.rotation+=PI
-	generate_field(huge_asteroid,Vector2(2100,313),-PI,80,0,0,start_time+0,1,Vector2(0,5))
+	generate_field(huge_asteroid,Vector2(2100,313),-PI,80,0,0,start_time+0,1,Vector2(0,5),Vector2(0.9,1.8))
 	generate_field(comet_scene,Vector2(-100,313),0,200,0.1,4,start_time+7,5,Vector2(0,100))
 	generate_field(comet_scene,Vector2(-100,313),0,450,0.1,5,start_time+8,7,Vector2(0,100))
 	generate_field(comet_scene,Vector2(-100,200),-PI/16,200,0.2,3,start_time+9,3,Vector2(0,50))
